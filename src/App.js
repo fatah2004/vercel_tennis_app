3a87fe382a13684e758dcd1106ba194c0c61552e
fatah <fkacm2004@gmail.com>
2023-12-29 23:42:04 +0100

    first change

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+         Ok now here are some new edits
         </a>
       </header>
     </div>
